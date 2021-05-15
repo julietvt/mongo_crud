f38eb8c983ddbe0da8476164040822b0c305c128
julietvt <freshitpt@gmail.com>
2021-05-15 12:04:57 +0300

    request for animals

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./router');
 const app = express();
+app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use('/api', router);
 module.exports = app;
